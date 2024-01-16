2810257e8862a5416b9863343470dd344e543f80
markw11 <mark.wang1@gmail.com>
2024-01-16 16:29:50 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -48,4 +48,8 @@ sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t-st
 sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-112m=y/d' .config
 sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-stock=y/d' .config
 
+sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY=y/d' .config
+sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y/d' .config
+sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Server=y/d' .config
+
 echo "CONFIG_PACKAGE_luci-app-smartdns=y" >> .config
