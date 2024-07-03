b25eadd7895d47c1d69c8cab506d303dc0e02d9f
markw11 <mark.wang1@gmail.com>
2024-07-03 18:07:28 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -21,8 +21,8 @@
 #rm -rf feeds/luci/applications/luci-app-passwall
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add luci-theme-argon
 #cd lede/package/lean
@@ -30,31 +30,31 @@
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 
-cp -f defconfig/mt7981-ax3000.config .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_abt_asr3000=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cetron_ct3003=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_a10=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m-emmc=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_h3c_nx30pro=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_imou_lc-hx3001=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_jcg_q30=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_konka_komi-a31=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_livinet_zr-3020=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-360-t7-108M=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-clt-r30b1=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t-stock=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-112m=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-stock=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt2500=y/d' .config
-sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt3000=y/d' .config
-
-#sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY=y/d' .config
-#sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y/d' .config
-#sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Server=y/d' .config
-
-echo "CONFIG_PACKAGE_luci-app-smartdns=y" >> .config
-echo "CONFIG_PACKAGE_curl=y" >> .config
-echo "CONFIG_PACKAGE_htop=y" >> .config
+#cp -f defconfig/mt7981-ax3000.config .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_abt_asr3000=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cetron_ct3003=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_a10=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m-emmc=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_h3c_nx30pro=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_imou_lc-hx3001=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_jcg_q30=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_konka_komi-a31=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_livinet_zr-3020=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-360-t7-108M=y/d' .config
+##sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-clt-r30b1=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t-stock=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-112m=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-stock=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt2500=y/d' .config
+#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt3000=y/d' .config
+#
+##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY=y/d' .config
+##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y/d' .config
+##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Server=y/d' .config
+#
+#echo "CONFIG_PACKAGE_luci-app-smartdns=y" >> .config
+#echo "CONFIG_PACKAGE_curl=y" >> .config
+#echo "CONFIG_PACKAGE_htop=y" >> .config
 
