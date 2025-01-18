e5a0bc8ca0197e47f4a51828ce4f270a390d0756
markw11 <mark.wang1@gmail.com>
2025-01-18 14:24:50 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -17,44 +17,32 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
 
+status_cfg=$(git status | grep -cE "feeds.conf.default$")
+if [[ $status_cfg -eq 1 ]]; then
+    git reset HEAD feeds.conf.default
+    git checkout feeds.conf.default
+fi
+
+\rm -rf ./tmp
+\rm -rf ./logs/*
+
+git pull
+
 # Add a feed source
+echo "src-git feeds_app https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/{luci-app-alist,luci-app-adguardhome,luci-app-ipsec-vpnd,luci-app-ipsec-vpnserver-manyusers,luci-app-smartdns,luci-app-mosdns,luci-app-passwall,luci-app-ssr-plus,luci-app-vssr} && rm -rf feeds/packages/net/{alist,adguardhome,brook,hysteria,mosdns,smartdns,chinadns-ng,v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin,shadowsocks-rust,trojan-go,trojan-plus,trojan}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 #rm -rf feeds/luci/applications/luci-app-passwall
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications
+#\rm -rf ./feeds/small/luci-app-homeproxy
+./scripts/feeds install -a
+#make menuconfig
 
 # Add luci-theme-argon
 #cd lede/package/lean
 #rm -rf luci-theme-argon 
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-
-#cp -f defconfig/mt7981-ax3000.config .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_abt_asr3000=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cetron_ct3003=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_a10=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m-emmc=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_rax3000m=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_h3c_nx30pro=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_imou_lc-hx3001=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_jcg_q30=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_konka_komi-a31=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_livinet_zr-3020=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-360-t7-108M=y/d' .config
-##sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_mt7981-clt-r30b1=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-ax3000t-stock=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-112m=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_xiaomi_mi-router-wr30u-stock=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt2500=y/d' .config
-#sed -i '/CONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_glinet_gl-mt3000=y/d' .config
-#
-##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NONE_V2RAY=y/d' .config
-##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Client=y/d' .config
-##sed -i '/CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Shadowsocks_NONE_Server=y/d' .config
-#
-#echo "CONFIG_PACKAGE_luci-app-smartdns=y" >> .config
-#echo "CONFIG_PACKAGE_curl=y" >> .config
-#echo "CONFIG_PACKAGE_htop=y" >> .config
-
