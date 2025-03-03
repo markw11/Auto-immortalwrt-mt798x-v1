a6b2b67be00f20d97714e4212f2248101ae5086e
markw11 <mark.wang1@gmail.com>
2025-03-03 19:28:28 +0800

    Update diy-part11.sh

--- script/diy-part11.sh
+++ script/diy-part11.sh
@@ -22,7 +22,7 @@
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add luci-theme-argon
 #cd lede/package/lean
