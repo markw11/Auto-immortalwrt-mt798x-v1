449d404054ce2277043a1c7c3af7c4a3c0f26e6b
markw11 <mark.wang1@gmail.com>
2025-03-20 12:11:58 +0800

    Update diy-part1.sh

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -28,6 +28,11 @@ fi
 
 git pull
 
+# Add missing feeds
+echo "src-git packages https://github.com/openwrt/packages" >> feeds.conf.default
+echo "src-git luci https://github.com/openwrt/luci" >> feeds.conf.default
+echo "src-git routing https://github.com/openwrt-routing/packages" >> feeds.conf.default
+echo "src-git telephony https://github.com/openwrt/telephony" >> feeds.conf.default
 # Add a feed source
 echo "src-git feeds_app https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
